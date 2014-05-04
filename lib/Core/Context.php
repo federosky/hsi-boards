160f0cfcd96dddd7bad7fdea35a25692bbffee05
federosky <federosky@gmail.com>
2014-05-04 19:27:54 -0300

    Lib Core refactor

---
+++ lib/Core/Context.php
@@ -0,0 +1,173 @@
+<?php
+/**
+ * Application run Context representation
+ */
+require_once(dirname(__FILE__).'/Session.php');
+
+/**
+ * Context
+ *
+ * @package    Libs
+ * @access private
+ */
+class Context
+{
+	/**
+	 * @var array
+	 * @access private
+	 */
+	private $_params  = array();
+
+	/**
+	 * @var Session
+	 * @access public
+	 */
+	public $session;
+
+	/**
+	 * Class constructor
+	 */
+	public function __construct()
+	{
+		if (!$_REQUEST && array_key_exists('QUERY_STRING', $_SERVER)) {
+			parse_str($_SERVER['QUERY_STRING'], $_REQUEST);
+		}
+		if (!$_REQUEST) {
+			$_REQUEST = array();
+		}
+
+		if (!array_key_exists('REQUEST_URI', $_SERVER)) {
+			$_SERVER['REQUEST_URI'] = '/';
+		}
+
+		$this->_params                             =  array_merge($_REQUEST, $_SERVER);
+		$this->_params['app.server']               =  $_SERVER;
+		$this->_params['app.request.params.get']   =  $_GET;
+		$this->_params['app.request.params.post']  =  $_POST;
+		$this->_params['app.request.cookies']      =  $_COOKIE;
+		$this->_params['app.request.params.files'] =  $_FILES;
+		$this->_params['app.request.body']         =  @file_get_contents('php://input');
+
+		$this->session = new Session();
+		$this->action($_SERVER['REQUEST_URI']);
+		$this->actionParams(array());
+
+		if (isset($_SERVER['PHP_AUTH_USER'])) $this->param('auth.username',  $_SERVER['PHP_AUTH_USER']);
+		if (isset($_SERVER['PHP_AUTH_PW']))   $this->param('auth.password',  $_SERVER['PHP_AUTH_PW']);
+	}
+
+	/**
+	 * Retorna la lista de parametros contenidas en el contexto
+	 *
+	 * @return array
+	 * {@source}
+	 */
+	public function params()
+	{
+		return $this->_params;
+	}
+
+	/**
+	 * Get params heys identifiers
+	 *
+	 * @return array
+	 */
+	public function paramsKeys()
+	{
+		return array_keys($this->_params);
+	}
+
+	/**
+	 * Accesor de Lectura/Escritura de los parametros del Contexto
+	 *
+	 * @param string $name El nombre del par�metro
+	 * @param mixed  [$value=null] El valor del par�metro
+	 * @return mixed
+	 * {@source}
+	 */
+	public function param($name, $value=null)
+	{
+		if (!is_null($value)) {
+			$this->_params[$name] = $value;
+		}
+
+		if (array_key_exists($name, $this->_params)) {
+			$return = $this->_params[$name];
+		} else {
+			$return = null;
+		}
+
+		return $return;
+	}
+
+	/**
+	 * Realiza un merge entre _params y el array pasado como argumento
+	 *
+	 * Retorna la lista de parametros contenidas en el contexto
+	 *
+	 * @param array $array
+	 * @return array
+	 * {@source}
+	 */
+	public function paramsMerge($array)
+	{
+		$array = is_array($array) ? $array : array($array);
+
+		$this->_params = array_merge($this->_params,$array);
+
+		return $this->params();
+	}
+
+	/**
+	 * M�todo utilizado para consultar las existencia de uno o m�s par�metros dentro del Contexto
+	 *
+	 * @param array|string $param
+	 * @return boolean
+	 */
+	public function hasParam($params)
+	{
+		$params = is_array($params) ? $params : array($params);
+		foreach ($params as $param) {
+			if (!array_key_exists($param, $this->_params)) return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * M�todo utilizado borrar un par�metro dentro del Contexto
+	 *
+	 * @param string $name
+	 * @return boolean
+	 * {@source}
+	 */
+	public function paramDelete($name)
+	{
+		unset($this->_params[$name]);
+	}
+
+	/**
+	 * Accesor de Lectura/Escritura de la accion a ejecutar
+	 *
+	 * @param mixed $value Valor de la Accion (Opcional)
+	 * @return string
+	 * {@source}
+	 */
+	public function action($value = null)
+	{
+		return $this->param('app.request.uri', $value);
+	}
+
+	/**
+	 * Accesor de Lectura/Escritura de los par�metros
+	 * extraidos de la acci�n
+	 *
+	 * @param mixed $value Valor de la Accion (Opcional)
+	 * @return string
+	 * {@source}
+	 */
+	public function actionParams($value=null)
+	{
+		return $this->param('app.request.uri.params', $value);
+	}
+}
---
+++ lib/Core/Session.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ * Session
+ *
+ * @package    Libs
+ * @subpackage Session
+ */
+class Session
+{
+	/**
+	 * @var String $_id  Session ID
+	 */
+	private $_id = 'hipodromo-hsi';
+
+	/**
+	 * @var String $_name  Session Name
+	 */
+	private $_name = 'pizarras';
+
+	/**
+	 * @var String $_savePath
+	 */
+	private $_savePath = '/tmp';
+
+	/**
+	 * @var array
+	 * @access private
+	 */
+	private $_params = null;
+
+	/**
+	 * Default Class constructor
+	 */
+	public function __construct($options = array())
+	{
+		if (!empty($options)) {
+			$this->setup($options);
+		}
+	}
+
+	/**
+	 * Setup Session config
+	 * @param Array $options
+	 * @throws Exception
+	 */
+	public function setup($options = array())
+	{
+		if (empty($options)) {
+			throw new Exception('Session config missing.');
+		}
+
+		if( !empty($options['id']) )
+			$this->id($options['id']);
+		if( !empty($options['name']) )
+			$this->name($options['name']);
+		if( !empty($options['save_path']) )
+			$this->savePath($options['save_path']);
+	}
+
+	/**
+	 * Retorna la lista de parametros contenidas en Session
+	 *
+	 * @return array
+	 */
+	public function params()
+	{
+		return array_keys($this->_params);
+	}
+
+	/**
+	 * Accesor de Lectura/Escritura de los parametros de la Sesion
+	 *
+	 * @param string $name El nombre del par�metro
+	 * @param mixed  $value El valor del par�metro (Opcional)
+	 *
+	 * @return mixed
+	 */
+	public function param($name, $value=null)
+	{
+		$this->open();
+		if (!is_null($value)) {
+			$this->_params[$name] = $value;
+		}
+
+		return $this->_params[$name];
+	}
+
+	/**
+	 * Check whether a param exists within the Session or not.
+	 *
+	 * @param string $name
+	 * @return boolean
+	 */
+	public function hasParam($params)
+	{
+		$this->open();
+
+		if (!is_array($params)) $params = array($params);
+		foreach ($params as $param) {
+			if (array_key_exists($param, $this->_params)) return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * M�todo utilizado borrar un par�metro dentro de la Sesion
+	 *
+	 * @param string $name
+	 * @return void
+	 */
+	public function delete($name)
+	{
+		$this->open();
+		unset($this->_params[$name]);
+	}
+
+	/**
+	 * Getter/Setter del Nombre de la Sesion
+	 *
+	 * @param string $name
+	 * @return void
+	 */
+	public function name($name=null)
+	{
+		if (!is_null($name)) {
+			session_name($name);
+		}
+
+		return session_name($this->_name);
+	}
+
+	/**
+	 * Getter/Setter del Id
+	 *
+	 * @param string id
+	 * @return string
+	 */
+	public function id($id = null)
+	{
+		if (!is_null($id)) {
+			session_id($id);
+		}
+		return session_id($this->_id);
+	}
+
+	/**
+	 * Getter/Setter del save_path
+	 *
+	 * @param string save_path
+	 * @return string
+	 */
+	public function savePath($path=null)
+	{
+		if (!is_null($path)) {
+			session_save_path($path);
+		}
+		return session_save_path($this->_savePath);
+	}
+
+	/**
+	 * M�todo utilizado para verificar se encuentra abierta Session
+	 *
+	 * @return boolean
+	 */
+	public function isOpen()
+	{
+		return !is_null($this->_params);
+	}
+
+	/**
+	 * M�todo utilizado iniciar la Session
+	 *
+	 * @return void
+	 */
+	public function open()
+	{
+		if (!$this->isOpen()) {
+			session_start();
+			$this->_params = $_SESSION;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * M�todo utilizado cerrar la Session
+	 *
+	 * @return void
+	 */
+	public function close()
+	{
+		session_write_close();
+		$this->_params = null;
+	}
+
+	/**
+	 * Destroy session instance
+	 */
+	public function destroy()
+	{
+		session_destroy();
+		$this->_params = null;
+	}
+
+}
